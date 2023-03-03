92ad22024abd9a4a360202cdf254454225755902
MoonGoon <moongoon.cnu@gmail.com>
2023-03-03 21:14:06 +0900

    Initial Commit

---
+++ OrderMate/OrderMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderMateApp.swift
+//  OrderMate
+//
+//  Created by 문영균 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct OrderMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
